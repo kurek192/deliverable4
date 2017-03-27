4d800a40c1d9e360d681180efe5c8199bf6714d7
William Kurek <wrk10@pitt.edu>
2017-03-27 12:55:24 -0400

    Update ProgramArea.java

--- src/ProgramArea.java
+++ src/ProgramArea.java
@@ -86,21 +86,17 @@ public class ProgramArea {
      * @param y y-coordinate
      * @return char The opcode (char) at that location
      */
+    
+    //REFACTOR
+	//removed unnecessary for loop
     public char getOpCode(int x, int y) {
 
         if (x >= _xSize || x < 0 || y >= _ySize || y < 0) {
             return (char) 0;
         }
 
-        //for (int j = -80000; j <= x; j++) {
-        //    for (int k = -80000; k <= y; k++) {
-        //	if (j == x && k == y) {
-        //	    return _area[j][k];
-        ///	}
-        //    }
-        //}
         return _area[x][y];
-        //return (char) 0;
+ 
     }
 
     /**
